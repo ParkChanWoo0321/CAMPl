3796f78d4a59d92d58a45f497a100b07524b114a
ParkChanWoo0321 <op9563_@naver.com>
2025-11-27 23:30:33 +0900

    장소 타입 추가

--- src/main/java/com/example/cample/place/domain/PlaceType.java
+++ src/main/java/com/example/cample/place/domain/PlaceType.java
@@ -2,7 +2,8 @@
 package com.example.cample.place.domain;
 
 public enum PlaceType {
-    RESTAURANT,  // 식당
-    CAFE,        // 카페
-    BAR          // 술집
+    RESTAURANT,      // 식당
+    CAFE,            // 카페
+    BAR,             // 술집
+    CAMPUS_BUILDING  // 교내 건물(청곡관, 영암체육관 등)
 }
